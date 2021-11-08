4671528e28988ecde47cd09db960b44918ea9383
qiuqian <qiu1641118989@163.com>
2021-11-08 16:18:08 +0800

    newbee-mall

---
+++ src/test/java/ltd/newbee/mall/newbeemall/NewbeeMallApplicationTests.java
@@ -0,0 +1,17 @@
+package ltd.newbee.mall.newbeemall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewbeeMallApplicationTests {
+	public static void main(String[] args) {
+		SpringApplication.run(NewbeeMallApplicationTests.class);
+	}
+	@Test
+	void contextLoads() {
+	}
+
+}
